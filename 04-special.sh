4383fb3edc7d5c56c23e2095e6bd29b6f0ea5338
kriiishamtic <kriiishmatic@gmail.com>
2025-09-20 12:22:23 +0530

    your commit message

--- 04-special.sh
+++ 04-special.sh
@@ -3,3 +3,6 @@
 echo " All variables passed to the script : $@ "
 echo " ABCD : $* "
 echo " script name : $0 "
+echo " current Woking dir : $PWD "
+echo " current user : $USER "
+echo " Users Home directory : $HOME "
