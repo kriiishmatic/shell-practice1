2d3c64c94adb80a54810dab21ff492820b64442a
kriiishamtic <kriiishmatic@gmail.com>
2025-09-22 15:21:56 +0530

    your commit message

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,8 +3,8 @@
 echo "Enter the number : "
 read Numb
 
-if [ $((Numb % 2)) -eq 0 ] then
-    echo " Number $Numb is even
+if [ $((Numb % 2)) -eq 0 ]; then
+    echo " Number $Numb is even "
 else
     echo " Number $Numb is odd "
 fi
