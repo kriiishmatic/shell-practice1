dbc5d3031ee1a1a86591c1eb777353fd2bfad1c8
kriiishamtic <kriiishmatic@gmail.com>
2025-09-22 15:50:22 +0530

    your commit message

--- 07-exit status.sh
+++ 07-exit status.sh
@@ -2,14 +2,16 @@
 
 userID=$(id -u)
 
-if [ $userID -ne 0 ] ; then
-    echo " Error you dont have sudo previleges"
+if [ $userID -ne 0 ]; then
+    echo " Error you dont have sudo previleges BOZO"
+    exit 2
 fi
  dnf install mysql -y
  
- if [ $? ne 0 ] ; then 
+ if [ $? ne 0 ]; then 
     echo " Error: MYSQL cannot be installed "
+    exit 2
 else
-    echo " Hurray! operation success "
+    echo " Hurray! operation success RIPBOZO"
 fi
 
