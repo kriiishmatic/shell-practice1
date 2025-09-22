6ceac22dce404311277967b512330e0076d86ea2
kriiishamtic <kriiishmatic@gmail.com>
2025-09-22 15:05:28 +0530

    your commit message

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -5,6 +5,8 @@
 Number=$1
 if [ $Number -lt 10 ] ; then
     echo " Given number $Number less than 10"
+elif [ $Number -eq 10 ] ; then 
+    echo " given number $Number equal to 10 "
 else
     echo " Given number $Number greather than or equal to 10 "
 
