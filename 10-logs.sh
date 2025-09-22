07bb0305162fafeea42cb224cd57b78dd5afdb84
kriiishamtic <kriiishmatic@gmail.com>
2025-09-22 23:47:59 +0530

    your commit message

--- 10-logs.sh
+++ 10-logs.sh
@@ -30,39 +30,39 @@ Remove_sh=$( echo $0 | cut -d "." -f1 )
 
 Logfile="$shell_log/$Remove_sh.log"
 
-echo " Script started at :: $(date) "
+echo " Script started at :: $(date) " | tee -a $Logfile
 
 
 Status(){
     if [ $1 -ne 0 ]; then
-    echo -e " $R Failed $N to install $2  "
+    echo -e " $R Failed $N to install $2  " | tee -a $Logfile
     exit 1
 else
-    echo -e "$G Sucessfully $N installed $2 "
+    echo -e "$G Sucessfully $N installed $2 " | tee -a $Logfile
 fi
 }
 
 dnf list installed nginx &>> $Logfile
  if [ $? -ne 0 ]; then
-    dnf install nginx -y &>> $Logfile
+    dnf install nginx -y &>> $Logfile | tee -a $Logfile
     Status $? "NGINX"
 else 
- echo -e " Already have it installed $Y skipping $N "
+ echo -e " Already have it installed $Y skipping $N " | tee -a $Logfile
  fi
 
 dnf list installed mysql &>> $Logfile
   if [ $? -ne 0 ]; then
-    dnf install mysql -y &>> $Logfile
+    dnf install mysql -y &>> $Logfile | tee -a $Logfile
     Status $? "MYSQLDDD"
 else
-    echo -e " Mysql insalled already sooo... $Y skipping $N " 
+    echo -e " Mysql insalled already sooo... $Y skipping $N "  | tee -a $Logfile
 
 fi
 
 dnf list installed python3-pip &>> $Logfile
     if [ $? -ne 0 ]; then
-    dnf install python3-pip -y & >>$Logfile
+    dnf install python3-pip -y & >>$Logfile | tee -a $Logfile
     Status $? "Python conda"
 else
-    echo -e " $Y skipping i cant "
+    echo -e " $Y skipping i cant " | tee -a $Logfile
 fi
