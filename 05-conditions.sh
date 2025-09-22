1a67a2f154a18077ed84de1daedf2376c0873203
kriiishamtic <kriiishmatic@gmail.com>
2025-09-22 15:01:11 +0530

    your commit message

---
+++ 05-conditions.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#conditions
+
+Number=$1
+if [ $Number -lt 10 ] ; then
+    echo " Given number $Number less than 10"
+else
+    echo " Given number $Number greather than or equal to 10 "
+
+fi
+
