66e7d93a5f541d77a283a7a2cd434bb529c70042
kriiishamtic <kriiishmatic@gmail.com>
2025-10-01 01:45:54 +0530

    Your commit message

--- 14-initialwhile.sh
+++ 14-initialwhile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-source_dir=/home/ec2-user/app_log
+source_dir=/home/ec2-user/app_logf
 
+if ( ! -d $source_dir ); then 
+    echo " $source_dir doesnot exist "
+    exit1
+    fi
 filetodelete=$(find $source_dir -name "*.log" -mtime +24)
 while IFS= read -r filebirth
     do
