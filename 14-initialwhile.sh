236b581ee29f9a24d22aad09c999dc25fb9a6b4e
kriiishamtic <kriiishmatic@gmail.com>
2025-10-01 01:47:56 +0530

    Your commit message

--- 14-initialwhile.sh
+++ 14-initialwhile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-source_dir=/home/ec2-user/app_logf
+source_dir=/home/ec2-user/app_log
 
 if ( ! -d $source_dir ); then 
     echo " $source_dir doesnot exist "
     exit1
+    else 
+    echo " Proceeding to FIND log files older than 14 days "
     fi
 filetodelete=$(find $source_dir -name "*.log" -mtime +24)
 while IFS= read -r filebirth
